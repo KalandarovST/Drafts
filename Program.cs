3c7c5a7ce80c102d0caa27cc739967ab91b9b326
KalandarovST <kalandarovtillo78@gmail.com>
2024-06-13 23:40:15 +0500

    6 lessson

--- Program.cs
+++ Program.cs
@@ -1,70 +1,24 @@
 ﻿using System;
-
-
-Console.Write("Bo`limni tanlang: ");
-string inputSon = Console.ReadLine();
-
-switch (inputSon)
+string userInput;
+do
 {
-   
-    case "1" :
-    { 
-    int x = 3;
-    int y = 6;
-    int qoshish = x + y;
-         Console.WriteLine("Arifmetigi= " + qoshish);
-    break;
-    }
-    case "2":
+    Console.WriteLine("Karra jadval");
+    int number = 1 ;
+    while (number <= 9)
     {
-        int z =6;
-        int f = 2;
-        int kopaytirish = z * f;
-            Console.WriteLine("Geometrigi= " + kopaytirish);
-    break;
-     }
-    default:
-    Console.WriteLine("Siz notog`ri tanladingiz");
-    break;
+        
+        Console.WriteLine($"1 x {number} = {number * 1}");
+        Console.WriteLine($"2 x {number} = {number * 2}");
+        Console.WriteLine($"3 x {number} = {number * 3}");
+        Console.WriteLine($"4 x {number} = {number * 4}");
+        Console.WriteLine($"5 x {number} = {number * 5}");
+        Console.WriteLine($"6 x {number} = {number * 6}");
+        Console.WriteLine($"7 x {number} = {number * 7}");
+        Console.WriteLine($"8 x {number} = {number * 8}");
+        Console.WriteLine($"9 x {number} = {number * 9}");
+        number++;
+    }
+    Console.WriteLine("Do you want continue? (y -> n)");
+    userInput = Console.ReadLine();
 }
-
-
-
-// using System;
-// Console.Write("Hafta kunini tanlang: ");
-// string inputKun = Console.ReadLine();
-
-// switch(inputKun)
-// {
-//     case "1":
-//     Console.WriteLine("Dushanba");
-//     break;
-
-//     case "2":
-//     Console.WriteLine("Seshanba");
-//     break;
-    
-//     case "3":
-//     Console.WriteLine("Chorshanba");
-//     break;
-
-//     case "4":
-//     Console.WriteLine("Payshanba");
-//     break;
-    
-//     case "5":
-//     Console.WriteLine("Juma");
-//     break;
-
-//     case "6":
-//     Console.WriteLine("Shanba");
-//     break;
-    
-//     case "7":
-//     Console.WriteLine("Yakshanba");
-//     break;
-    
-//     default:
-//     Console.WriteLine("Unday kun yo`q");
-//     break;
-// }
+while(userInput == "y");
